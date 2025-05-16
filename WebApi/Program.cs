935111ac420a3f08acb0ffa568cacd8294e055f8
Tobias0801 <tobias.kuhn22@icloud.com>
2025-05-16 08:46:59 +0200

    finish yml file

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,28 @@
+using MongoDB.Driver;
+using MongoDB.Bson;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var movieDatabaseConficSection = builder.Configuration.GetSection("DatabaseSettings");
+builder.Services.Configure<DatabaseSettings>(movieDatabaseConficSection);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Minimal API Version 1.0");
+
+app.MapGet("/check", (Microsoft.Extensions.Options.IOptions<DatabaseSettings> options) => {
+
+    try {
+        var connectionString = options.Value.ConnectionString;
+        var client = new MongoClient(connectionString);
+        var databases = client.ListDatabaseNames().ToList();
+        return "Access to MongoDB ok. Existing Databases: " + string.Join(", ", databases);
+    }
+    catch (System.Exception e)
+    {
+        return "Acces to MongoDB doesn't work: " + e.Message;
+    }
+    
+});
 
 app.Run();
